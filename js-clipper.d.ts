02d1e1a521f148755daa5c9aac7804b9cbb8718a
Rupertofly <rupertqmoore@me.com>
2019-02-22 15:07:12 +1100

    added simple offset definition

--- js-clipper.d.ts
+++ js-clipper.d.ts
@@ -23,14 +23,42 @@ declare module 'js-clipper' {
         ptSubject,
         ptClip
     }
+  export enum JoinType {
+      jtSquare,jtRound,jtMiter
+  }
+  export enum EndType {
+    etOpenSquare,
+    etOpenRound,
+    etOpenButt,
+    etClosedLine,
+    etClosedPolygon
+  }
+
+    export namespace JS {
+        function Clean(path: path, amount: number): path;
+        function Clean(path: paths, amount: number): paths;
+    }
     export class Clipper {
+        public static SimplifyPolygons(
+            paths: paths,
+            fillType: PolyFillType
+        ): paths;
+        public static CleanPolygon(path: path, amount: number): path;
+        public static CleanPolygons(path: paths, amount: number): paths;
         constructor();
         public AddPath(path: path, type: PolyType, closed: boolean): void;
         public AddPaths(paths: paths, type: PolyType, closed: boolean): void;
         public Execute(
             clipType: ClipType,
             solution: path | paths,
-            fillType?: PolyFillType
+            subFillType?: PolyFillType,
+            clipFillType?: PolyFillType
         ): boolean;
     }
+  export class ClipperOffset {
+    constructor();
+    public AddPath( path: path, join: JoinType, end: EndType ):void;
+    public AddPaths( path: paths, join: JoinType, end: EndType ): void;
+    public Execute( solution: path | paths, amount: number ):void;
+  }
 }
