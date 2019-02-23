d13e3979b616659330d7800dd0004897df72a437
Rupertofly <rupertqmoore@me.com>
2019-02-24 10:27:58 +1100

    simplify and change to function form

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 
 // P5 Sketch
 import p5 from 'p5';
-import bs from '../modules/bSpline';
 import Sketch from './sketch';
-const instance = new p5( is => {
-  console.log(bs.toString());
-
-  Object.entries(new Sketch(is)).map(f => {
-    is[f[0]] = f[1];
-  });
-});
+const instance = new p5( Sketch);
