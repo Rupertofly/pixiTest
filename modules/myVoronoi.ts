ca33cbb4ea382d9677cc9753125d2e0a0724a2e0
Rupertofly <rupertqmoore@me.com>
2019-02-24 16:26:48 +1100

    add relax

--- modules/myVoronoi.ts
+++ modules/myVoronoi.ts
@@ -1,25 +1,50 @@
-import Pol from 'd3-polygon';
-import Vor from 'd3-voronoi';
+import *  as Pol from 'd3-polygon';
+import * as Vor from 'd3-voronoi';
 import _ from 'lodash';
 import p5 from 'p5';
 
 type myPG = Vor.VoronoiPolygon<VorCell>;
 
 export class VorDiagram {
-    public layoutFunction: Vor.VoronoiLayout<{ x: number; y: number }>;
+    public layoutFunction: Vor.VoronoiLayout<VorCell>;
     public count: number;
     public cells: VorCell[] = [];
-    constructor (width: number, height: number, count?: number, random?: boolean) {
-        this.layoutFunction = Vor.voronoi<{ x: number; y: number }>()
-      .size([width, height])
-      .x((a: any) => a.x)
-      .y((a: any) => a.y);
+    public diagram: Vor.VoronoiDiagram<VorCell>;
+    constructor(
+        width: number,
+        height: number,
+        count?: number,
+        random?: boolean
+    ) {
+        this.layoutFunction = Vor.voronoi<VorCell>()
+            .size([width, height])
+            .x((a: any) => a.x)
+            .y((a: any) => a.y);
         this.count = count || Math.floor((width * height) / 30 ** 2);
         for (let i = 0; i < this.count; i++) {
             this.cells.push(
-        new VorCell(_.random(width, true), _.random(height, true), i.toString())
-      );
+                new VorCell(
+                    _.random(width, true),
+                    _.random(height, true),
+                    i.toString()
+                )
+            );
         }
+        this.diagram = this.layoutFunction(this.cells);
+        this.refresh();
+    }
+    public refresh() {
+        this.diagram = this.layoutFunction(this.cells);
+        this.diagram.polygons().map(polygon => {
+            let cell = polygon.data;
+            cell.setPolygon(polygon);
+        });
+    }
+    public relax(count: number) {
+        for (let i = 0; i < count; i++) {
+            this.cells.forEach( cell => cell.relaxCell() );
+        }
+        this.refresh();
     }
 }
 export class VorCell {
@@ -29,13 +54,13 @@ export class VorCell {
     public identifier: string;
     public colour?: string;
 
-    constructor (x: number, y: number, i: string) {
+    constructor(x: number, y: number, i: string) {
         this.position = new p5.Vector().set(x, y);
         this.identifier = i;
     }
-    public setPos (posArr: pt): this;
-    public setPos (x: number, y: number): this;
-    public setPos (...args: Array<number | pt>) {
+    public setPos(posArr: pt): this;
+    public setPos(x: number, y: number): this;
+    public setPos(...args: Array<number | pt>) {
         if (args.length > 1) {
             this.position.set(args[0] as number, args[1] as number);
             return this;
@@ -44,19 +69,22 @@ export class VorCell {
         this.position.set(point[0] as number, point[1] as number);
         return this;
     }
-    get x () {
+    get x() {
         return this.position.x;
     }
-    get y () {
+    get y() {
         return this.position.y;
     }
-    public getCentroid () {
+    public getCentroid() {
         if (!this.polygon) throw new Error('No Polygon Available');
         this.centroid = Pol.polygonCentroid(this.polygon);
         return this.centroid;
     }
-    public setPolygon (pgon: Vor.VoronoiPolygon<VorCell>) {
+    public setPolygon(pgon: Vor.VoronoiPolygon<VorCell>) {
         this.polygon = pgon;
         return this;
+    }
+    public relaxCell() {
+        return this.setPos(this.getCentroid());
     }
 }
